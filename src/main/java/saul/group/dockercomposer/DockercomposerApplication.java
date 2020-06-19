197b3467bd898bd2a4a7cb4f087a056255adad31
Saul Feliciano <jmartinez@intenpro.com>
2020-06-19 16:29:21 -0400

    Initial commit

---
+++ src/main/java/saul/group/dockercomposer/DockercomposerApplication.java
@@ -0,0 +1,13 @@
+package saul.group.dockercomposer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockercomposerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockercomposerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/saul/group/dockercomposer/DockercomposerApplicationTests.java
@@ -0,0 +1,13 @@
+package saul.group.dockercomposer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockercomposerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
